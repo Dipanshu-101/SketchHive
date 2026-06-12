e812348523c153d4bae90a97d3709b2d230c9c80
Dipanshu <deshmukhdipanshu101@gmail.com>
2026-06-12 19:21:53 +0000

    basic end point of ws server and https server along with auth logic and middleware gaurding ws is done

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,29 @@
 import express from 'express';
-
+import jwt from 'jsonwebtoken';     
+import { JWT_SECRET } from './config';
+import { middleware } from './middleware';
 const app = express();
 
+app.post("/signup", (req, res) => {
+    // I have to add zod validation here to validate the request body
+    res.json({
+        userId: 1,  //this will come from the database after creating the user (no need to send as response)
+        message: "User created successfully"
+    })
+});
+
+app.post("/signin", (req, res) => {
+        const userId = 1; // this will come from the database after validating the user credentials
+        const token = jwt.sign({
+            userId
+        },JWT_SECRET);
+
+        res.json({ token });
+});
+
+app.post("/room", middleware , (req, res) => {
+    //db call
+    res.json({ message: "Room created successfully" });
+    
+});
 app.listen(3001);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,13 +1,32 @@
 import { WebSocketServer } from 'ws';
-
+import jwt from 'jsonwebtoken';
 const wss = new WebSocketServer({ port: 8080 });
+import { JWT_SECRET } from './config';
+wss.on('connection', function connection(ws , request) {
+  
+  const url = request.url;
+    if (!url) {
+    ws.close();
+    return;
+    }
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
-
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+  const token = queryParams.get('token') || '';
+  const decoded = jwt.verify(token,JWT_SECRET);
+  ///@ts-ignore
+       if (!decoded|| !decoded.userId) {
+    ws.close();
+    return;
+  }
   ws.on('message', function message(data) {
     console.log('received: %s', data);
   });
 
-  ws.send('something');
 });
+
+
+//think wss as a whole server of websocket layer and ws is a single connection to a client. Each time a client connects, a new ws object is created for that connection, allowing you to handle messages and events specific to that client. The wss object manages all the connections and can broadcast messages to all clients if needed.
+
+
+
+//Browser WebSocket clients cannot send custom Authorization headers, so JWT authentication is commonly implemented either through query parameters during the handshake or by sending an authentication message immediately after the connection is established. Node.js clients can use custom headers because they have low-level control over the HTTP handshake.
