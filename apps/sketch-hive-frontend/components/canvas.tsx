25c26967e8ca9d5ba74d7751b624cb9cb6670b95
Dipanshu <deshmukhdipanshu101@gmail.com>
2026-06-20 17:53:50 +0000

    Refactored WS-BE

--- apps/sketch-hive-frontend/components/canvas.tsx
+++ apps/sketch-hive-frontend/components/canvas.tsx
@@ -1,15 +1,34 @@
 "use client"
-import {useEffect , useRef } from "react";
+import {useEffect , useRef, useState} from "react";
 import initDraw from "@/draw";
+import { WS_BACKEND_URL } from "@/config";
 
 export function Canvas ({roomId}: {roomId: string}) {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+//@ts-ignore
+    useEffect(() => {
+        const ws = new WebSocket(WS_BACKEND_URL);
+        
+        ws.onopen = () => {
+            setSocket(ws);
+         }
+        
+         }  , []);
+
 
     useEffect(() => {
         if (canvasRef.current) {
             initDraw(canvasRef.current, roomId);
         }
         }, [canvasRef]);
+
+    if (!socket) { 
+
+            return <div>waiting for socket to connect...
+                 </div>
+
+            }
     return (
         <div >
             <canvas ref={canvasRef} width={2800} height={1080} ></canvas>
