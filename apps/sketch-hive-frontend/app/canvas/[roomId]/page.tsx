45bcf5dfd266263c82811da683e128db90815ff3
Dipanshu <deshmukhdipanshu101@gmail.com>
2026-06-23 18:04:42 +0000

    FE: added signin page

--- apps/sketch-hive-frontend/app/canvas/[roomId]/page.tsx
+++ apps/sketch-hive-frontend/app/canvas/[roomId]/page.tsx
@@ -1,14 +1,12 @@
 import { RoomCanvas } from "@/components/RoomCanvas";
 
-
 export default async function Canvaspage( {params}: {
     params: {
         roomId: string
     }}) {
 
     const roomId = (await params).roomId; 
-       
-    
+         
 return <RoomCanvas roomId={roomId} />
 
 
