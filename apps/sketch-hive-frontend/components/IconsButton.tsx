b6c27b2d75f6e0831ca062aade76e2041f5dc92a
Dipanshu <deshmukhdipanshu101@gmail.com>
2026-06-23 18:08:45 +0000

    FK1

--- apps/sketch-hive-frontend/components/IconsButton.tsx
+++ apps/sketch-hive-frontend/components/IconsButton.tsx
@@ -6,7 +6,8 @@ export function IconButton({
     icon: ReactNode,
     onClick: () => void,
     activated: boolean
-}) {
+}) 
+{
     return <div className={`m-2 pointer rounded-full border p-2 bg-black hover:bg-gray ${activated ? "text-red-400" : "text-white"}`} onClick={onClick}>
         {icon}
     </div>
