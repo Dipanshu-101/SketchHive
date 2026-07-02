c8767760412e9ee1e0c1b7de0b6e38f7d7be954f
Dipanshu <deshmukhdipanshu101@gmail.com>
2026-07-02 02:46:08 +0000

    Build AuthLayout + AppShell layout shells + wire route-group layouts

--- packages/icons/eslint.config.mjs
+++ packages/icons/eslint.config.mjs
@@ -1,5 +1,4 @@
 import { config } from "@repo/eslint-config/react-internal";
-import { noRawHex } from "@repo/eslint-config/no-raw-hex";
 
 /** @type {import("eslint").Linter.Config} */
-export default [...config, ...noRawHex];
+export default config;
